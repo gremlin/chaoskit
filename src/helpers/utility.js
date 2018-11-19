c955f254907d59f6df207227fd265322a4f2e7ae
Zach Schnackel <info@zslabs.com>
2018-11-19 16:39:23 -0500

    fix mobile error

--- src/helpers/utility.js
+++ src/helpers/utility.js
@@ -98,17 +98,7 @@ export function directoryUrl(url) {
  * @return {Boolean} [description]
  */
 export function isTouchDevice() {
-  const prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
-  const mq = query => window.matchMedia(query).matches;
-
-  if ((('ontouchstart' in window) || window.DocumentTouch) && document instanceof window.DocumentTouch) {
-    return true;
-  }
-
-  // include the 'heartz' as a way to have a non matching MQ to help terminate the join
-  // https://git.io/vznFH
-  const query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
-  return mq(query);
+  return 'ontouchstart' in document.documentElement;
 }
 
 /**
