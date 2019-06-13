43c227c72d0edfc4584e7549856edca636376a6f
Zach Schnackel <info@zslabs.com>
2019-06-13 10:47:26 -0400

    list item class

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,3 +1,12 @@
-const ListItem = ({ ...opts }) => <li {...opts} />;
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+
+const ListItem = ({ className, ...opts }) => (
+  <li className={cx('CK__ListItem', className)} {...opts} />
+);
+
+ListItem.propTypes = {
+  className: PropTypes.string,
+};
 
 export default ListItem;
