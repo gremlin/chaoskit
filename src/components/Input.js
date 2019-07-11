bfd2c3d7730d158b486cc698e823e4dd727e0501
Zach Schnackel <info@zslabs.com>
2019-07-10 22:50:37 -0400

    input updates

--- src/components/Input.js
+++ src/components/Input.js
@@ -10,6 +10,50 @@ import Icon from './Icon';
 import { form } from '../assets/styles/utility';
 import { generateUUID } from '../helpers/utility';
 
+const StylesInputBase = (theme, props = {}) => [
+  form.base(theme),
+  {
+    //  Remove default style in browsers that support `appearance`
+    appearance: 'none',
+  },
+
+  // Apply default form styling, except for `file`, `submit`, `reset`, `button` and `image`
+  !['file', 'submit', 'reset', 'button', 'image'].includes(props.type) &&
+    form.input(theme),
+
+  // Fix the cursor style for Chrome's increment/decrement buttons. For certain `font-size` values of the `input`, it causes the cursor style of the decrement button to change from `default` to `text`.
+  props.type === 'number' && {
+    '&::-webkit-inner-spin-button, &::-webkit-outer-spin-button': {
+      height: 'auto',
+    },
+  },
+
+  // Remove inner padding and search cancel button in Chrome, Safari and Opera on OS X.
+  props.type === 'search' && {
+    '&::-webkit-search-cancel-button, &::-webkit-search-decoration': {
+      appearance: 'none',
+    },
+  },
+  {
+    // 1. Correct the inability to style clickable types in iOS and Safari.
+    // 2. Change font properties to `inherit` in Safari.
+    '&::--webkit-file-upload-button': {
+      appearance: 'button', // 1
+      font: 'inherit', // 2
+    },
+
+    // Remove clear button in IE on inputs
+    '&::ms-clear': {
+      display: 'none',
+    },
+
+    // Removes `box-shadow` for invalid controls in Firefox.
+    '&:invalid': {
+      boxShadow: 'none',
+    },
+  },
+];
+
 const Input = forwardRef(
   (
     {
@@ -46,54 +90,26 @@ const Input = forwardRef(
 
       // `react-text-mask` does not support 'email' or 'number' input types
       if (mask && !['email', 'number'].includes(type)) {
-        return <MaskedInput {...defaultProps} mask={mask} guide={guide} />;
+        return (
+          <MaskedInput
+            {...defaultProps}
+            mask={mask}
+            guide={guide}
+            render={(maskRef, props) => (
+              <input
+                css={theme => StylesInputBase(theme, defaultProps)}
+                className={cx('CK__Input', className)}
+                ref={input => maskRef(input)}
+                {...props}
+              />
+            )}
+          />
+        );
       }
 
       return (
         <input
-          css={theme => [
-            form.base(theme),
-            {
-              //  Remove default style in browsers that support `appearance`
-              appearance: 'none',
-            },
-
-            // Apply default form styling, except for `file`, `submit`, `reset`, `button` and `image`
-            !['file', 'submit', 'reset', 'button', 'image'].includes(type) &&
-              form.input(theme),
-
-            // Fix the cursor style for Chrome's increment/decrement buttons. For certain `font-size` values of the `input`, it causes the cursor style of the decrement button to change from `default` to `text`.
-            type === 'number' && {
-              '&::-webkit-inner-spin-button, &::-webkit-outer-spin-button': {
-                height: 'auto',
-              },
-            },
-
-            // Remove inner padding and search cancel button in Chrome, Safari and Opera on OS X.
-            type === 'search' && {
-              '&::-webkit-search-cancel-button, &::-webkit-search-decoration': {
-                appearance: 'none',
-              },
-            },
-            {
-              // 1. Correct the inability to style clickable types in iOS and Safari.
-              // 2. Change font properties to `inherit` in Safari.
-              '&::--webkit-file-upload-button': {
-                appearance: 'button', // 1
-                font: 'inherit', // 2
-              },
-
-              // Remove clear button in IE on inputs
-              '&::ms-clear': {
-                display: 'none',
-              },
-
-              // Removes `box-shadow` for invalid controls in Firefox.
-              '&:invalid': {
-                boxShadow: 'none',
-              },
-            },
-          ]}
+          css={theme => StylesInputBase(theme, defaultProps)}
           className={cx('CK__Input', className)}
           {...defaultProps}
         />
