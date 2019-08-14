41d7989732abb3488d4afe5aef89fc2a106ec8cb
Zach Schnackel <info@zslabs.com>
2019-08-14 15:42:36 -0400

    add contrast utility to close

--- src/components/Close.js
+++ src/components/Close.js
@@ -7,28 +7,37 @@ export const StylesCloseVariables = theme => ({
   size: theme.fontSize.base,
 });
 
-const Close = ({ onClick, ...opts }) => (
+const Close = ({ onClick, noContrast, ...opts }) => (
   <Button
     type="reset"
     title="Close"
     onClick={onClick}
-    css={theme => ({
-      // 1. To align better with headers
-      color: theme.fontColor.base,
-      lineHeight: theme.lineHeight.small, // 1
-      width: StylesCloseVariables(theme).size,
-      height: StylesCloseVariables(theme).size,
-      opacity: theme.opacity.base,
-      transition: `opacity ${theme.timing.base} ${theme.transition.base}`,
+    css={theme => [
+      {
+        // 1. To align better with headers
+        color: theme.fontColor.base,
+        lineHeight: theme.lineHeight.small, // 1
+        width: StylesCloseVariables(theme).size,
+        height: StylesCloseVariables(theme).size,
+        opacity: theme.opacity.base,
+        transition: `opacity ${theme.timing.base} ${theme.transition.base}`,
 
-      '&:hover, &:focus': {
-        opacity: 1,
+        '&:hover, &:focus': {
+          opacity: 1,
 
-        '.CK__Close__Icon': {
-          transform: 'scale(1.15)',
+          '.CK__Close__Icon': {
+            transform: 'scale(1.15)',
+          },
         },
       },
-    })}
+
+      theme.settings.contrast.enable &&
+        !noContrast && {
+          '.u-contrast &': {
+            color: theme.contrast.base,
+          },
+        },
+    ]}
     {...opts}
   >
     <Icon
@@ -44,6 +53,7 @@ const Close = ({ onClick, ...opts }) => (
 
 Close.propTypes = {
   onClick: PropTypes.func,
+  noContrast: PropTypes.bool,
 };
 
 export default Close;
--- src/components/Close.stories.js
+++ src/components/Close.stories.js
@@ -2,7 +2,12 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
 import { Close } from '.';
+import Contrast from '../../.storybook/components/Contrast';
 
-storiesOf('Components|Close', module).add('Overview', () => (
-  <Close onClick={action('Clicked')} />
-));
+storiesOf('Components|Close', module)
+  .add('Overview', () => <Close onClick={action('Clicked')} />)
+  .add('Contrast', () => (
+    <Contrast>
+      <Close onClick={action('Clicked')} />
+    </Contrast>
+  ));
