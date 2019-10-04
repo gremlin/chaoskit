8f114742254fcd022dc8b5d8816d8ee5434aed24
Zach Schnackel <info@zslabs.com>
2019-10-04 17:04:59 -0400

    ckcache to function

--- src/helpers/Wrapper.js
+++ src/helpers/Wrapper.js
@@ -8,40 +8,40 @@ import { theme } from '../assets/styles/theme';
 import { fonts } from '../assets/styles/fonts';
 import { globalStyles } from '../assets/styles/global';
 
-export const ckCache = createCache({
-  key: 'ck',
-  // Only prefix the following style properties
-  prefix: key => {
-    switch (key) {
-      case 'appearance':
-      case 'backdrop-filter':
-      case 'box-decoration-break':
-      case 'clip-path':
-      case 'mask-border-outset':
-      case 'mask-border-repeat':
-      case 'mask-border-slice':
-      case 'mask-border-source':
-      case 'mask-border-width':
-      case 'mask-border':
-      case 'mask-clip':
-      case 'mask-composite':
-      case 'mask-image':
-      case 'mask-origin':
-      case 'mask-position':
-      case 'mask-repeat':
-      case 'mask-size':
-      case 'mask':
-      case 'text-emphasis-color':
-      case 'text-emphasis-position':
-      case 'text-emphasis-style':
-      case 'text-emphasis':
-        return true;
-      default:
-        return false;
-    }
-  },
-  // stylisPlugins: [stylisCalc],
-});
+export const ckCache = () =>
+  createCache({
+    key: 'ck',
+    // Only prefix the following style properties
+    prefix: key => {
+      switch (key) {
+        case 'appearance':
+        case 'backdrop-filter':
+        case 'box-decoration-break':
+        case 'clip-path':
+        case 'mask-border-outset':
+        case 'mask-border-repeat':
+        case 'mask-border-slice':
+        case 'mask-border-source':
+        case 'mask-border-width':
+        case 'mask-border':
+        case 'mask-clip':
+        case 'mask-composite':
+        case 'mask-image':
+        case 'mask-origin':
+        case 'mask-position':
+        case 'mask-repeat':
+        case 'mask-size':
+        case 'mask':
+        case 'text-emphasis-color':
+        case 'text-emphasis-position':
+        case 'text-emphasis-style':
+        case 'text-emphasis':
+          return true;
+        default:
+          return false;
+      }
+    },
+  });
 
 const Wrapper = ({ children }) => (
   <CacheProvider value={ckCache}>
