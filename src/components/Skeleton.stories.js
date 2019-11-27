6da08304466f7f7970a4d96a667363258babb4ec
Zach Schnackel <info@zslabs.com>
2019-11-26 22:15:35 -0500

    skeleton contrast styles

--- src/components/Skeleton.stories.js
+++ src/components/Skeleton.stories.js
@@ -1,10 +1,19 @@
 import { storiesOf } from '@storybook/react';
 
 import { Inline, Skeleton } from '.';
+import Contrast from '../../.storybook/components/Contrast';
 
-storiesOf('Components|Skeleton', module).add('Overview', () => (
+const SkeletonExample = () => (
   <Inline>
     <Skeleton css={{ width: 200, height: 50 }} />
     <Skeleton css={{ width: 50, height: 50, borderRadius: '50%' }} />
   </Inline>
-));
+);
+
+storiesOf('Components|Skeleton', module)
+  .add('Overview', () => <SkeletonExample />)
+  .add('Contrast', () => (
+    <Contrast>
+      <SkeletonExample />
+    </Contrast>
+  ));
