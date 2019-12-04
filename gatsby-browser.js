d9976fdd6975e63455895f76509f5aae929059a1
Zach Schnackel <info@zslabs.com>
2019-12-04 17:36:11 -0500

    Update deps, remove default smooth scroll behavior

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,12 +3,3 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
-
-// The following two functions are used to trigger a specific class used to attach the `scroll-behavior` modifier; to avoid unecessary smooth-scrolling to the top of the page between routes
-exports.onPreRouteUpdate = () => {
-  document.documentElement.classList.remove('is-loaded');
-};
-
-exports.onRouteUpdate = () => {
-  document.documentElement.classList.add('is-loaded');
-};
