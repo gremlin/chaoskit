13d0858ad64770ae20ab1ba1e999b6189928bfba
Zach Schnackel <info@zslabs.com>
2019-12-12 07:59:50 -0500

    mooooar

--- src/components/NotificationProvider.js
+++ src/components/NotificationProvider.js
@@ -1,42 +1,21 @@
-import { Fragment, createContext, useReducer, useRef } from 'react';
+import {
+  Fragment,
+  forwardRef,
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useRef,
+} from 'react';
 import PropTypes from 'prop-types';
 import { createPortal } from 'react-dom';
 import { useTheme } from 'emotion-theming';
+import gsap from 'gsap';
 
 import { generateUUID } from '../helpers/utility';
 import Close from './Close';
 
-const Notification = ({ children, color, onRemove }) => {
-  const theme = useTheme();
-
-  return (
-    <div
-      className="u-contrast"
-      css={{
-        background: theme.color[color].base,
-        padding: theme.space.base,
-        borderRadius: theme.borderRadius.base,
-        color: theme.contrast.muted,
-      }}
-    >
-      {children}
-      <Close onClick={onRemove} />
-    </div>
-  );
-};
-
-Notification.propTypes = {
-  children: PropTypes.node.isRequired,
-  color: PropTypes.oneOf(['primary', 'danger']),
-  onRemove: PropTypes.func.isRequired,
-};
-
-Notification.defaultProps = {
-  color: 'primary',
-};
-
 const NotificationContext = createContext();
-let notificationRef;
 
 const initialState = [];
 
@@ -61,33 +40,120 @@ const reducer = (state, action) => {
   }
 };
 
+const Notification = forwardRef(({ children, color }, ref) => {
+  const theme = useTheme();
+
+  return (
+    <div
+      className="u-contrast"
+      css={{
+        background: theme.color[color].base,
+        padding: theme.space.base,
+        borderRadius: theme.borderRadius.base,
+        color: theme.contrast.muted,
+
+        // GSAP
+        visibility: 'hidden',
+      }}
+      ref={ref}
+    >
+      {children}
+      <Close
+        onClick={() => {
+          ref.current.timeline.reverse();
+        }}
+      />
+    </div>
+  );
+});
+
+Notification.propTypes = {
+  children: PropTypes.node.isRequired,
+  color: PropTypes.oneOf(['primary', 'danger']),
+};
+
+Notification.defaultProps = {
+  color: 'primary',
+};
+
+const NotificationWrapper = ({ notification }) => {
+  const { dispatch } = useContext(NotificationContext);
+  const theme = useTheme();
+
+  const notificationRef = useRef();
+
+  useEffect(() => {
+    notificationRef.current.timeline = gsap.timeline({
+      yoyo: true,
+      onReverseComplete() {
+        dispatch({
+          type: 'remove',
+          payload: notification.index,
+        });
+      },
+    });
+
+    notificationRef.current.timeline
+      .set(notificationRef.current, {
+        visibility: 'visible',
+        marginBottom: theme.space.small,
+        marginTop: -notificationRef.current.offsetHeight,
+        opacity: theme.opacity.base,
+      })
+      .to(notificationRef.current, {
+        duration: theme.gsap.timing.base,
+        ease: theme.gsap.transition.base,
+        opacity: 1,
+        marginTop: 0,
+      });
+  }, []);
+
+  return (
+    <Notification color={notification.color} ref={notificationRef}>
+      {notification.content} {notification.index}
+    </Notification>
+  );
+};
+
+NotificationWrapper.propTypes = {
+  notification: PropTypes.object.isRequired,
+};
+
 const NotificationProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
   const value = { state, dispatch };
 
-  notificationRef = useRef();
+  const theme = useTheme();
 
   return (
     <NotificationContext.Provider value={value}>
       <Fragment>
         {createPortal(
           <div
-            ref={notificationRef}
-            css={{ position: 'fixed', top: 0, right: 0 }}
+            css={{
+              position: 'fixed',
+              top: theme.space.small,
+              right: theme.space.small,
+              zIndex: 10,
+              overflowY: 'auto',
+              scrollBarWidth: 'none',
+              msOverflowStyle: 'none',
+              WebkitOverflowScrolling: 'touch',
+
+              '&::-webkit-scrollbar': {
+                width: 0,
+              },
+
+              [theme.mq.small]: {
+                width: 300,
+              },
+            }}
           >
             {state.map(notification => (
-              <Notification
+              <NotificationWrapper
                 key={notification.index}
-                color={notification.color}
-                onRemove={() =>
-                  dispatch({
-                    type: 'remove',
-                    payload: notification.index,
-                  })
-                }
-              >
-                {notification.content} {notification.index}
-              </Notification>
+                notification={notification}
+              />
             ))}
           </div>,
           document.body
