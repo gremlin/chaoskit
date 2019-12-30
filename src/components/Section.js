4eb23e665132ca72524e65f3ac4c5d763721d0e1
Zach Schnackel <info@zslabs.com>
2019-12-30 15:37:14 -0500

    Add "as" prop to Section component

--- src/components/Section.js
+++ src/components/Section.js
@@ -10,70 +10,72 @@ const StylesSectionVariables = {
   slantOffset: 2.5,
 };
 
-const Section = forwardRef(({ space, slant, className, ...opts }, ref) => {
-  const theme = useTheme();
+const Section = forwardRef(
+  ({ as: Component, space, slant, className, ...opts }, ref) => {
+    const theme = useTheme();
 
-  return (
-    <section
-      ref={ref}
-      className={cx('CK__Section', className)}
-      css={[
-        misc.trimChildren,
+    return (
+      <Component
+        ref={ref}
+        className={cx('CK__Section', className)}
+        css={[
+          misc.trimChildren,
 
-        misc.fluidSize({
-          theme,
-          property: 'paddingTop',
-          from: theme.space[space],
-          to: theme.space[space] * 2,
-        }),
-        misc.fluidSize({
-          theme,
-          property: 'paddingBottom',
-          from: theme.space[space],
-          to: theme.space[space] * 2,
-        }),
+          misc.fluidSize({
+            theme,
+            property: 'paddingTop',
+            from: theme.space[space],
+            to: theme.space[space] * 2,
+          }),
+          misc.fluidSize({
+            theme,
+            property: 'paddingBottom',
+            from: theme.space[space],
+            to: theme.space[space] * 2,
+          }),
 
-        slant === 'top' && {
-          clipPath: `polygon(
+          slant === 'top' && {
+            clipPath: `polygon(
             0 0,
             100% ${StylesSectionVariables.slantOffset}vw,
             100% 100%,
             0 calc(100% - ${StylesSectionVariables.slantOffset}vw)
           )`,
-          marginTop: `-${StylesSectionVariables.slantOffset}vw`,
-        },
+            marginTop: `-${StylesSectionVariables.slantOffset}vw`,
+          },
 
-        (slant === 'bottom' || slant === 'bottom-shadow') && [
-          {
-            clipPath: `polygon(
+          (slant === 'bottom' || slant === 'bottom-shadow') && [
+            {
+              clipPath: `polygon(
           0 0,
           100% 0,
           100% 100%,
           0 calc(100% - ${StylesSectionVariables.slantOffset}vw)
         )`,
-          },
-
-          slant === 'bottom-shadow' && {
-            position: 'relative',
-
-            '&::after': {
-              content: "''",
-              position: 'absolute',
-              right: 0,
-              left: 0,
-              bottom: `-${StylesSectionVariables.slantOffset}vw`,
-              zIndex: -1,
-              borderBottom: `${StylesSectionVariables.slantOffset *
-                2.25}vw solid ${rgba(theme.color.dark.base, 0.03)}`,
-              filter: 'blur(5px)',
             },
-          },
-        ],
-      ]}
-      {...opts}
-    />
-  );
-});
+
+            slant === 'bottom-shadow' && {
+              position: 'relative',
+
+              '&::after': {
+                content: "''",
+                position: 'absolute',
+                right: 0,
+                left: 0,
+                bottom: `-${StylesSectionVariables.slantOffset}vw`,
+                zIndex: -1,
+                borderBottom: `${StylesSectionVariables.slantOffset *
+                  2.25}vw solid ${rgba(theme.color.dark.base, 0.03)}`,
+                filter: 'blur(5px)',
+              },
+            },
+          ],
+        ]}
+        {...opts}
+      />
+    );
+  }
+);
 
 Section.propTypes = {
   space: PropTypes.oneOf([
@@ -90,6 +92,10 @@ Section.propTypes = {
 
 Section.defaultProps = {
   space: 'large',
+};
+
+Section.defaultProps = {
+  as: 'section',
 };
 
 export default Section;
