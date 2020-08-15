f316afd94f4f6ba9ea78d039d403119e1e64696c
Zach Schnackel <info@zslabs.com>
2020-08-14 22:38:01 -0400

    more updates

---
+++ src/components/Checkbox.story.js
@@ -0,0 +1,22 @@
+import ContrastWrapper from '../../.storybook/components/ContrastWrapper'
+
+import Checkbox from './Checkbox'
+
+export default {
+  title: 'Forms/Checkbox',
+  component: Checkbox,
+  args: {
+    label: 'Checkbox',
+    name: 'checkbox',
+    value: 'value',
+  },
+  argTypes: { onChange: { action: 'Toggled' } },
+}
+
+const Story = (args) => <Checkbox {...args} />
+
+export const Overview = Story.bind({})
+
+export const Contrast = Story.bind({})
+
+Contrast.decorators = [(story) => <ContrastWrapper>{story()}</ContrastWrapper>]
---
+++ src/components/Toggle.story.js
@@ -0,0 +1,22 @@
+import ContrastWrapper from '../../.storybook/components/ContrastWrapper'
+
+import Toggle from './Toggle'
+
+export default {
+  title: 'Forms/Toggle',
+  component: Toggle,
+  args: {
+    label: 'Form label',
+    name: 'toggle',
+    value: 'value',
+  },
+  argTypes: { onChange: { action: 'Toggled' } },
+}
+
+const Story = (args) => <Toggle {...args} />
+
+export const Overview = Story.bind({})
+
+export const Contrast = Story.bind({})
+
+Contrast.decorators = [(story) => <ContrastWrapper>{story()}</ContrastWrapper>]
