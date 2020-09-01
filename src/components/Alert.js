5160a0a68deb415d721a286bfc3c1317c7232ffe
Zach Schnackel <info@zslabs.com>
2020-09-01 00:04:56 -0400

    alert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,50 +1,38 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef } from 'react'
 import clsx from 'clsx'
 import PropTypes from 'prop-types'
 import useUpdateEffect from 'react-use/lib/useUpdateEffect'
-import gsap from 'gsap'
+import { motion, useAnimation, AnimatePresence } from 'framer-motion'
 import { useTheme } from 'emotion-theming'
 
 import { misc, text } from '../assets/styles/utility'
 
 import Close from './Close'
 
-export const StylesAlertBase = (theme, opts) => {
-  const props = {
-    ...{
-      close: false,
-    },
-    ...opts,
-  }
+export const StylesAlertBase = (theme) => [
+  misc.fluidSize({
+    theme,
+    property: 'padding',
+    from: theme.space.base,
+    to: theme.space.large,
+  }),
+  {
+    borderLeft: '8px solid transparent',
+    color: theme.fontColor.base,
+    borderRadius: theme.borderRadius.base,
 
-  return [
-    misc.fluidSize({
-      theme,
-      property: 'padding',
-      from: theme.space.base,
-      to: theme.space.large,
-    }),
-    {
-      display: 'grid',
-      gridTemplateColumns: props.close && '1fr auto',
-      gap: props.close && theme.space.small,
-      borderLeft: '8px solid transparent',
-      color: theme.fontColor.base,
-      borderRadius: theme.borderRadius.base,
+    'a:not([class]), a[class=""], .u-link': [
+      text.underline,
+      {
+        color: 'currentColor',
 
-      'a:not([class]), a[class=""], .u-link': [
-        text.underline,
-        {
+        '&:hover, &:focus': {
           color: 'currentColor',
-
-          '&:hover, &:focus': {
-            color: 'currentColor',
-          },
         },
-      ],
-    },
-  ]
-}
+      },
+    ],
+  },
+]
 
 export const StylesAlertDefault = (theme) => ({
   borderColor: theme.color.border.base,
@@ -73,147 +61,108 @@ export const StylesAlertDanger = (theme) => ({
 const Alert = ({
   children,
   className,
-  close,
-  collapse = false,
+  reveal,
+  setReveal,
   onComplete = () => {},
   onReverseComplete = () => {},
-  onReverseStart = () => {},
-  onStart = () => {},
   title,
   type = 'default',
   ...rest
 }) => {
   const theme = useTheme()
 
-  const alertRef = useRef()
-  const [hidden, setHidden] = useState(false)
+  const ref = useRef()
 
-  const handleOnComplete = () => {
-    setHidden(true)
+  const direction = useRef('reverse')
 
-    onComplete()
-  }
+  const controls = useAnimation()
 
-  const handleOnReverseStart = () => {
-    setHidden(false)
-
-    onReverseStart()
-  }
-
-  const attachTimeline = () => {
-    const $alert = alertRef.current
-
-    let forward = true
-    let lastTime = 0
-
-    // Attach GSAP
-    $alert.timeline = gsap.timeline({
-      paused: true,
-      onStart: () => {
-        onStart()
-      },
-      onUpdate: () => {
-        const newTime = $alert.timeline.time()
-
-        if (
-          (forward && newTime < lastTime) ||
-          (!forward && newTime > lastTime)
-        ) {
-          forward = !forward
-          if (!forward) {
-            handleOnReverseStart()
-          }
-        }
-        lastTime = newTime
-      },
-      onComplete: () => {
-        handleOnComplete()
-      },
-      onReverseComplete: () => {
-        onReverseComplete()
-      },
-    })
-
-    $alert.timeline.to($alert, {
-      duration: theme.gsap.timing.long,
-      marginTop: -$alert.offsetHeight,
-      transformOrigin: 'center center',
-      yPercent: 50,
-      autoAlpha: 0,
-      ease: theme.gsap.transition.base,
-    })
-
-    if (collapse) {
-      $alert.timeline.progress(1)
-    }
-  }
-
-  const collapseAlert = () => {
-    const $alert = alertRef.current
-
-    if ($alert && $alert.timeline) $alert.timeline.play()
-  }
-
-  const openAlert = () => {
-    const $alert = alertRef.current
-
-    if ($alert && $alert.timeline) $alert.timeline.reverse()
+  const variants = {
+    hidden: {
+      opacity: 0,
+      y: '-100%',
+      height: 0,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      height: 'auto',
+    },
   }
 
   useEffect(() => {
-    attachTimeline()
+    if (reveal) {
+      controls.set('visible')
+      direction.current = 'forward'
+    }
   }, [])
 
   useUpdateEffect(() => {
-    if (collapse) {
-      collapseAlert()
+    if (reveal) {
+      controls.start('visible')
+      direction.current = 'forward'
     } else {
-      openAlert()
+      controls.start('hidden')
+      direction.current = 'reverse'
     }
-  }, [collapse])
+  }, [reveal])
 
   return (
-    <div
-      css={[
-        StylesAlertBase(theme, {
-          close,
-        }),
+    <AnimatePresence onExitComplete={onReverseComplete} initial={false}>
+      {reveal && (
+        <motion.div
+          css={[
+            StylesAlertBase(theme),
 
-        hidden && misc.hide,
+            typeof setReveal === 'function' && {
+              display: 'grid',
+              gridTemplateColumns: '1fr auto',
+              gap: theme.space.small,
+            },
 
-        type === 'default' && StylesAlertDefault(theme),
-        type === 'primary' && StylesAlertPrimary(theme),
-        type === 'warning' && StylesAlertWarning(theme),
-        type === 'danger' && StylesAlertDanger(theme),
-      ]}
-      className={clsx('CK__Alert', className)}
-      role="alert"
-      ref={alertRef}
-      aria-hidden={hidden ? 'true' : 'false'}
-      {...rest}
-    >
-      <div className={`CK__Alert__Content ${theme.settings.classes.trim}`}>
-        {title && <h4 className="CK__Alert__Title">{title}</h4>}
-        {children}
-      </div>
-      {close && <Close className="CK__Alert__Close" onClick={collapseAlert} />}
-    </div>
+            type === 'default' && StylesAlertDefault(theme),
+            type === 'primary' && StylesAlertPrimary(theme),
+            type === 'warning' && StylesAlertWarning(theme),
+            type === 'danger' && StylesAlertDanger(theme),
+          ]}
+          className={clsx('CK__Alert', className)}
+          role="alert"
+          ref={ref}
+          variants={variants}
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+          transition={{ ease: 'easeInOut' }}
+          onAnimationComplete={() =>
+            direction.current === 'forward' && onComplete()
+          }
+          {...rest}
+        >
+          <div className={`CK__Alert__Content ${theme.settings.classes.trim}`}>
+            {title && <h4 className="CK__Alert__Title">{title}</h4>}
+            {children}
+          </div>
+          {typeof setReveal === 'function' && (
+            <Close
+              className="CK__Alert__Close"
+              onClick={() => setReveal(false)}
+            />
+          )}
+        </motion.div>
+      )}
+    </AnimatePresence>
   )
 }
 
 Alert.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
-  collapse: PropTypes.bool,
-  /** GSAP callback */
+  /** Animation callback */
   onComplete: PropTypes.func,
-  /** GSAP callback */
+  /** Animation callback */
   onReverseComplete: PropTypes.func,
-  /** GSAP callback */
-  onReverseStart: PropTypes.func,
-  /** GSAP callback */
-  onStart: PropTypes.func,
-  close: PropTypes.bool,
+  reveal: PropTypes.bool,
+  setReveal: PropTypes.func.isRequired,
   title: PropTypes.string,
   type: PropTypes.oneOf(['default', 'primary', 'warning', 'danger']),
 }
