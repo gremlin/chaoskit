6a1d8a10240f8185364b5d0fb959f2cfd36c0d90
Zach Schnackel <info@zslabs.com>
2020-10-02 22:40:29 -0400

    Hook form

---
+++ src/components/FormField.js
@@ -0,0 +1,60 @@
+import { useMemo } from 'react'
+import PropTypes from 'prop-types'
+import { useFormContext } from 'react-hook-form'
+
+import Input from './Input'
+import Textarea from './Textarea'
+import Select from './Select'
+import Radio from './Radio'
+import Checkbox from './Checkbox'
+import Toggle from './Toggle'
+
+const FormField = ({ as = 'input', name, ...rest }) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext()
+
+  const Component = useMemo(() => {
+    // eslint-disable-next-line default-case
+    switch (as) {
+      case 'input':
+        return Input
+      case 'textarea':
+        return Textarea
+      case 'select':
+        return Select
+      case 'radio':
+        return Radio
+      case 'checkbox':
+        return Checkbox
+      case 'toggle':
+        return Toggle
+    }
+  }, [as])
+
+  const componentProps = {
+    validationMessage: errors[name]?.message ?? '',
+  }
+
+  // Remove `errorText` prop from fields that don't support `validationMessage` to avoid DOM warning
+  if (['radio', 'checkbox', 'toggle'].includes(as)) {
+    delete componentProps.validationMessage
+  }
+
+  return <Component ref={register} name={name} {...componentProps} {...rest} />
+}
+
+FormField.propTypes = {
+  as: PropTypes.oneOf([
+    'input',
+    'textarea',
+    'select',
+    'radio',
+    'checkbox',
+    'toggle',
+  ]),
+  name: PropTypes.string.isRequired,
+}
+
+export default FormField
--- src/components/HookForm.stories.js
+++ src/components/HookForm.stories.js
@@ -1,12 +1,13 @@
-import { Form, Formik } from 'formik'
+import { useForm, FormProvider } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers'
 import * as yup from 'yup'
 
 import Button from './Button'
 import FormGroup from './FormGroup'
-import FormikField from './FormikField'
+import FormField from './FormField'
 
 export default {
-  title: 'Forms/Formik',
+  title: 'Forms/Hook Form',
   parameters: {
     controls: { hideNoControlsWarning: true },
   },
@@ -19,31 +20,32 @@ const schema = yup.object().shape({
 })
 
 const Story = () => {
+  const methods = useForm({
+    defaultValues: {
+      name: '',
+      email: '',
+      message: '',
+    },
+    resolver: yupResolver(schema),
+  })
+
   const handleSubmit = (values) => {
     console.log(values) // eslint-disable-line no-console
   }
 
   return (
-    <Formik
-      initialValues={{
-        name: '',
-        email: '',
-        message: '',
-      }}
-      validationSchema={schema}
-      onSubmit={handleSubmit}
-    >
-      <Form>
-        <FormikField name="name" label="Name" required />
-        <FormikField type="email" name="email" label="Email" required />
-        <FormikField as="textarea" name="message" label="Message" required />
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(handleSubmit)} noValidate>
+        <FormField name="name" label="Name" required />
+        <FormField type="email" name="email" label="Email" required />
+        <FormField as="textarea" name="message" label="Message" required />
         <FormGroup>
           <Button type="primary" actionType="submit">
             Submit
           </Button>
         </FormGroup>
-      </Form>
-    </Formik>
+      </form>
+    </FormProvider>
   )
 }
 
