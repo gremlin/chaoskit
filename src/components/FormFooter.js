a37fc0e38058fcffd018d4baec9903e28425e54a
Zach Schnackel <info@zslabs.com>
2020-10-03 22:53:15 -0400

    installation page update

--- src/components/FormFooter.js
+++ src/components/FormFooter.js
@@ -1,82 +1,81 @@
 import PropTypes from 'prop-types'
 import clsx from 'clsx'
 import { useTheme } from 'emotion-theming'
+import { forwardRef } from 'react'
 
-const FormFooter = ({
-  className,
-  explanationMessage,
-  validationMessage,
-  ...rest
-}) => {
-  const theme = useTheme()
+const FormFooter = forwardRef(
+  ({ className, explanationMessage, validationMessage, ...rest }, ref) => {
+    const theme = useTheme()
 
-  return explanationMessage || validationMessage ? (
-    <div
-      css={[
-        {
-          fontSize: theme.fontSize.small,
-          lineHeight: theme.lineHeight.small,
+    return explanationMessage || validationMessage ? (
+      <div
+        ref={ref}
+        css={[
+          {
+            fontSize: theme.fontSize.small,
+            lineHeight: theme.lineHeight.small,
 
-          [theme.mq.medium]: {
-            display: 'flex',
-            width: '100%',
+            [theme.mq.medium]: {
+              display: 'flex',
+              width: '100%',
+            },
           },
-        },
-      ]}
-      className={clsx('CK__FormFooter', className)}
-      {...rest}
-    >
-      {explanationMessage && (
-        <div
-          css={[
-            {
-              color: theme.fontColor.muted,
-              marginTop: theme.space.xsmall,
+        ]}
+        className={clsx('CK__FormFooter', className)}
+        {...rest}
+      >
+        {explanationMessage && (
+          <div
+            css={[
+              {
+                color: theme.fontColor.muted,
+                marginTop: theme.space.xsmall,
 
-              [theme.mq.medium]: {
-                '&:not(:only-child)': {
-                  marginRight: theme.space.small,
+                [theme.mq.medium]: {
+                  '&:not(:only-child)': {
+                    marginRight: theme.space.small,
+                  },
                 },
               },
-            },
 
-            theme.settings.contrast.enable &&
-              theme.settings.contrast.form && {
-                '.u-contrast &': {
-                  color: theme.contrast.base,
+              theme.settings.contrast.enable &&
+                theme.settings.contrast.form && {
+                  '.u-contrast &': {
+                    color: theme.contrast.base,
+                  },
+                },
+            ]}
+          >
+            {explanationMessage}
+          </div>
+        )}
+        {validationMessage && (
+          <div
+            css={[
+              {
+                color: theme.color.danger.base,
+                marginTop: theme.space.xsmall,
+
+                [theme.mq.medium]: {
+                  marginLeft: 'auto',
                 },
               },
-          ]}
-        >
-          {explanationMessage}
-        </div>
-      )}
-      {validationMessage && (
-        <div
-          css={[
-            {
-              color: theme.color.danger.base,
-              marginTop: theme.space.xsmall,
 
-              [theme.mq.medium]: {
-                marginLeft: 'auto',
-              },
-            },
-
-            theme.settings.contrast.enable &&
-              theme.settings.contrast.form && {
-                '.u-contrast &': {
-                  color: theme.contrast.base,
+              theme.settings.contrast.enable &&
+                theme.settings.contrast.form && {
+                  '.u-contrast &': {
+                    color: theme.contrast.base,
+                  },
                 },
-              },
-          ]}
-        >
-          {validationMessage}
-        </div>
-      )}
-    </div>
-  ) : null
-}
+            ]}
+          >
+            {validationMessage}
+          </div>
+        )}
+      </div>
+    ) : null
+  }
+)
 
 FormFooter.propTypes = {
   className: PropTypes.string,
