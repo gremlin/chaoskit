f1320c772371549b0a4bb18f3a1aa7b9b9e30f26
Zach Schnackel <info@zslabs.com>
2020-10-05 11:59:16 -0400

    Add color and typography stories

---
+++ src/components/Colors.stories.js
@@ -0,0 +1,80 @@
+import PropTypes from 'prop-types'
+import { useTheme } from 'emotion-theming'
+import { Fragment } from 'react'
+
+import ListItem from './ListItem'
+import Inline from './Inline'
+
+export default {
+  title: 'General/Color',
+}
+
+const ColorBlock = ({ title, ...rest }) => {
+  const theme = useTheme()
+
+  return (
+    <div
+      css={{
+        borderRadius: theme.borderRadius.large,
+        border: theme.border.base,
+        boxShadow: theme.boxShadow.base,
+        padding: theme.space.xsmall,
+      }}
+    >
+      <div
+        css={{
+          height: theme.height.large,
+          borderRadius: theme.borderRadius.large,
+        }}
+        {...rest}
+      />
+      <div
+        css={{
+          marginTop: theme.space.small,
+          fontFamily: theme.fontFamily.code,
+        }}
+      >
+        {title}
+      </div>
+    </div>
+  )
+}
+
+ColorBlock.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+const Story = () => {
+  const theme = useTheme()
+
+  return Object.entries(theme.color).map(([label, value]) => {
+    return (
+      <Fragment key={label}>
+        <h5>{label}</h5>
+        <Inline>
+          {Object.entries(value).map(([colorLabel, colorValue]) => {
+            return (
+              <ListItem key={colorValue}>
+                <ColorBlock
+                  title={colorLabel}
+                  css={[
+                    colorLabel.includes('filter') && {
+                      background: '#000',
+                      filter: theme.color[label][colorLabel],
+                    },
+
+                    !colorLabel.includes('filter') && {
+                      background: theme.color[label][colorLabel],
+                    },
+                  ]}
+                />
+              </ListItem>
+            )
+          })}
+        </Inline>
+      </Fragment>
+    )
+  })
+}
+
+export const Overview = Story.bind({})
---
+++ src/components/Typography.stories.js
@@ -0,0 +1,126 @@
+import PropTypes from 'prop-types'
+import { useTheme } from 'emotion-theming'
+import clsx from 'clsx'
+
+import { text } from '../assets/styles/utility'
+
+export default {
+  title: 'General/Typography',
+}
+
+const TypographyColumn = ({ className, title, children, ...rest }) => {
+  const theme = useTheme()
+
+  return (
+    <div
+      className={clsx(theme.settings.classes.trim, className)}
+      css={{
+        borderRadius: theme.borderRadius.large,
+        border: theme.border.base,
+        boxShadow: theme.boxShadow.base,
+      }}
+      {...rest}
+    >
+      <header
+        className="u-contrast"
+        css={[
+          text.heading(theme),
+          {
+            background: theme.color.dark.base,
+            borderTopLeftRadius: theme.borderRadius.large,
+            borderTopRightRadius: theme.borderRadius.large,
+            padding: theme.space.xsmall,
+            textAlign: 'center',
+            textTransform: 'uppercase',
+            fontSize: theme.fontSize.small,
+            letterSpacing: theme.letterSpacing.small,
+          },
+        ]}
+      >
+        {title}
+      </header>
+      <div
+        css={{
+          padding: theme.space.base,
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  )
+}
+
+TypographyColumn.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  title: PropTypes.string.isRequired,
+}
+
+const Story = () => {
+  const theme = useTheme()
+
+  return (
+    <div
+      css={{
+        display: 'grid',
+        gap: theme.space.base,
+
+        [theme.mq.medium]: {
+          gridTemplateColumns: 'repeat(2, 1fr)',
+        },
+      }}
+    >
+      <TypographyColumn title="Headings">
+        {['h1', 'h2', 'h3', 'h4', 'h5'].map((heading) => {
+          const Component = heading
+
+          return <Component key={heading}>Heading {heading}</Component>
+        })}
+      </TypographyColumn>
+      <TypographyColumn title="Font sizes">
+        {Object.entries(theme.fontSize).map(([label, value]) => {
+          return (
+            <p
+              key={label}
+              css={[
+                typeof value === 'object' && [value],
+
+                typeof value !== 'object' && {
+                  fontSize: value,
+                },
+              ]}
+            >
+              {label}
+            </p>
+          )
+        })}
+      </TypographyColumn>
+      <TypographyColumn title="Font colors">
+        {Object.entries(theme.fontColor).map(([label, value]) => {
+          return (
+            <p
+              key={label}
+              css={[
+                typeof value === 'object' && [value],
+
+                typeof value !== 'object' && [
+                  label.includes('filter') && {
+                    filter: value,
+                  },
+
+                  !label.includes('filter') && {
+                    color: value,
+                  },
+                ],
+              ]}
+            >
+              {label}
+            </p>
+          )
+        })}
+      </TypographyColumn>
+    </div>
+  )
+}
+
+export const Overview = Story.bind({})
