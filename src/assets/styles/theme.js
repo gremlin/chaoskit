440eed4db962e6a937a8fd17f1940136aa795182
Zach Schnackel <info@zslabs.com>
2020-11-02 09:06:04 -0500

    revert base box shadow to previous

--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -164,7 +164,7 @@ const borderRadius = {
 
 // eslint-disable-next-line camelcase
 const BETA__boxShadow = {
-  base: '0 1px 3px 0 rgba(0,0,0,.06), 0 1px 2px 0 rgba(0,0,0,.04)',
+  base: 'rgba(0,0,0,0.16) 0px 1px 5px -2px',
   medium: '0 4px 6px -1px rgba(0,0,0,.08), 0 2px 4px -1px rgba(0,0,0,.04)',
   large: '0 10px 15px -3px rgba(0,0,0,.08), 0 4px 6px -2px rgba(0,0,0,.04)',
   xlarge: '0 20px 25px -5px rgba(0,0,0,.08), 0 10px 10px -5px rgba(0,0,0,.04)',
